45736d2faf5e7da111778b53b665660c1407a9eb
SabaPivot <careforme.dropout@gmail.com>
2024-10-25 15:24:21 +0900

    Fix: Black linting and annotation

--- function.py
+++ function.py
@@ -3,11 +3,15 @@ import time
 import os
 from utils import handle_dialog, login_and_cache, find_file
 
+
 # 카드 정보 오류 확인
 class PaymentVerificationError(Exception):
     pass
 
-AUTH_FILE = "auth_state.json"  # Path to save the cached authentication state
+
+# 로그인 캐시 파일 경로
+AUTH_FILE = "auth_state.json"
+
 
 def execute_drive(card_info):
     with sync_playwright() as p:
@@ -18,37 +22,48 @@ def execute_drive(card_info):
         if os.path.exists(AUTH_FILE):
             print("이전 로그인 정보로 로그인을 시도합니다.")
             context = browser.new_context(storage_state=AUTH_FILE)
+
         # 캐시 파일이 존재하지 않으면 수동으로 로그인
         else:
-            print("이전 로그인 정보를 찾지 못했습니다. 수동으로 로그인해주세요. 20초 후에 자동화 프로그램이 시작됩니다.")
+            print(
+                "이전 로그인 정보를 찾지 못했습니다. 수동으로 로그인해주세요. 20초 후에 자동화 프로그램이 시작됩니다."
+            )
             context = browser.new_context()
 
         page = context.new_page()
 
         # 캐시로 웹사이트 접속하기
         try:
-            page.goto("https://service.epost.go.kr/front.commonpostplus.RetrieveAcceptPlus.postal?gubun=1")
+            page.goto(
+                "https://service.epost.go.kr/front.commonpostplus.RetrieveAcceptPlus.postal?gubun=1"
+            )
+
             # 로그인 성공했는지 확인
-            member_info_visible = page.is_visible('div.memberInfo')
+            member_info_visible = page.is_visible("div.memberInfo")
             if not member_info_visible:
                 raise Exception("로그인이 필요합니다.")
 
         except Exception as e:
-            print(f"로그인이 실패했습니다. 수동으로 로그인해주세요. 20초 후에 자동화 프로그램이 시작됩니다.: {str(e)}")
+            print(
+                f"로그인이 실패했습니다. 수동으로 로그인해주세요. 20초 후에 자동화 프로그램이 시작됩니다.: {str(e)}"
+            )
             login_and_cache(context, page, AUTH_FILE)
 
-        page.goto("https://service.epost.go.kr/front.commonpostplus.RetrieveAcceptPlus.postal?gubun=1")
+        # 로그인 끝나면 다시 페이지로 이동
+        page.goto(
+            "https://service.epost.go.kr/front.commonpostplus.RetrieveAcceptPlus.postal?gubun=1"
+        )
 
         # 보내는분 정보 수정 (이름 입력)
-        page.fill('#tSndNm', '')
-        page.fill('#tSndNm', '블랙독')
+        page.fill("#tSndNm", "")
+        page.fill("#tSndNm", "블랙독")
 
         # 배송 정보 수신 (SMS)
-        page.click('#recevYn4')
+        page.click("#recevYn4")
 
         # 팝업 창 popup_info로 받아주기
         with page.expect_popup() as popup_info:
-            page.click('#tSpecialDivCd4')  # '준등기' 버튼 클릭하기
+            page.click("#tSpecialDivCd4")  # '준등기' 버튼 클릭하기
 
         # 팝업창 핸들링 (닫기)
         popup = popup_info.value
@@ -57,24 +72,23 @@ def execute_drive(card_info):
         # 우체통 접수 서비스
         # 팝업 창 popup_info로 받아주기
         with page.expect_popup() as popup_info:
-            page.click("#mBoxThwrY2")
+            page.click("#mBoxThwrY2")  # 우체통 접수 신청하기
 
         # 팝업창 핸들링 (닫기)
         popup = popup_info.value
         popup.close()
 
         # 내용물 코드 '510' 선택
-        page.select_option('#semiCode', '510')
-
+        page.select_option("#semiCode", "510")
 
         # 업로드할 주소 파일 찾기 (1개만!)
         file_to_upload = find_file()
 
         # 주소(파일)이용하기
         with page.expect_popup() as popup_info:
-            page.click('#btnUseAddrFile')
+            page.click("#btnUseAddrFile")
             popup = popup_info.value
-            popup.set_input_files('#uploadFile', file_to_upload)
+            popup.set_input_files("#uploadFile", file_to_upload)
 
         # 카드 번호 입력
         card1 = card_info["card1"]
@@ -88,63 +102,71 @@ def execute_drive(card_info):
         credit_birth = card_info["credit_birth"]
 
         # 카드 정보 입력
-        credit_no_input1 = page.locator('#creditNo1')
+        credit_no_input1 = page.locator("#creditNo1")
         credit_no_input1.scroll_into_view_if_needed()
         credit_no_input1.fill(card1)
 
-        credit_no_input1 = page.locator('#creditNo2')
+        credit_no_input1 = page.locator("#creditNo2")
         credit_no_input1.scroll_into_view_if_needed()
         credit_no_input1.fill(card2)
 
-        credit_no_input1 = page.locator('#creditNo3')
+        credit_no_input1 = page.locator("#creditNo3")
         credit_no_input1.scroll_into_view_if_needed()
         credit_no_input1.fill(card3)
 
-        credit_no_input1 = page.locator('#creditNo4')
+        credit_no_input1 = page.locator("#creditNo4")
         credit_no_input1.scroll_into_view_if_needed()
         credit_no_input1.fill(card4)
 
-        creditExp1 = page.locator('#creditExp1')
-        creditExp2 = page.locator('#creditExp2')
+        creditExp1 = page.locator("#creditExp1")
+        creditExp2 = page.locator("#creditExp2")
         creditExp1.fill(month)
         creditExp2.fill(year)
 
-        creditPwd1 = page.locator('#creditPwd1')
-        creditPwd2 = page.locator('#creditPwd2')
+        creditPwd1 = page.locator("#creditPwd1")
+        creditPwd2 = page.locator("#creditPwd2")
         creditPwd1.fill(pass1)
         creditPwd2.fill(pass2)
 
-        creditBirth = page.locator('#creditBirth1')
+        creditBirth = page.locator("#creditBirth1")
         creditBirth.fill(credit_birth)
 
         # 결제카드검증
-        page.wait_for_selector('#certCreditInfo')  # Wait for the button to appear
+        page.wait_for_selector("#certCreditInfo")  # Wait for the button to appear
         time.sleep(1)
-        page.click('#certCreditInfo')
+        page.click("#certCreditInfo")
         time.sleep(1)
-        page.click('#certCreditInfo')
+        page.click("#certCreditInfo")
 
-        error_locator1 = page.locator('li#mag4.red_b')
-        error_locator2 = page.locator('li#mag5.red_b')
-        error_locator3 = page.locator('li#mag6.red_b')
+        error_locator1 = page.locator("li#mag4.red_b")
+        error_locator2 = page.locator("li#mag5.red_b")
+        error_locator3 = page.locator("li#mag6.red_b")
 
         if error_locator1.is_visible():
-            raise PaymentVerificationError("Error: 결제카드 검증에 실패했습니다. 결제카드정보를 확인해주세요.")
+            raise PaymentVerificationError(
+                "Error: 결제카드 검증에 실패했습니다. 결제카드정보를 확인해주세요."
+            )
 
         if error_locator2.is_visible():
-            raise PaymentVerificationError("Error: 현재 검증서비스를 이용할 수 없습니다.")
+            raise PaymentVerificationError(
+                "Error: 현재 검증서비스를 이용할 수 없습니다."
+            )
 
         if error_locator3.is_visible():
-            raise PaymentVerificationError("Error: 결제카드 검증에 실패했습니다. 네트워크 상태를 확인해주세요.")
+            raise PaymentVerificationError(
+                "Error: 결제카드 검증에 실패했습니다. 네트워크 상태를 확인해주세요."
+            )
 
         # 개인정보 수집 동의
         # 스크롤 끝까지 내리고 '동의합니다' 체크
-        page.wait_for_selector('#agreeBox')  # Wait until the element is available
-        page.evaluate("""() => {
+        page.wait_for_selector("#agreeBox")  # Wait until the element is available
+        page.evaluate(
+            """() => {
             var element = document.getElementById('agreeBox');
             element.scrollIntoView();  // Scroll the box into view
             element.scrollTop = element.scrollHeight;  // Scroll inside the box to the bottom
-        }""")
+        }"""
+        )
         page.click("#applyAgree_Priv")
 
         # 접수 신청
@@ -153,21 +175,28 @@ def execute_drive(card_info):
         # 주소 검증 팝업 (자동 처리)
 
         # 다시 접수 신청
-        page.click('#btnReqClick')
-        page.click('#btnReqClick')
+        page.click("#btnReqClick")
+        page.click("#btnReqClick")
         # Wait for the notification to appear
-        page.wait_for_selector('#notiContentDiv.show')
+        page.wait_for_selector("#notiContentDiv.show")
 
-        # Wait for the close button to be visible
-        confirm_button = page.wait_for_selector('a.btn_m.bg_red#confirmBtn', state='visible')
+        # 접수 신청하면 새로운 팝업이 열림
+        confirm_button = page.wait_for_selector(
+            "a.btn_m.bg_red#confirmBtn", state="visible"
+        )
 
-        # Click the close button
+        # 최종 확인 버튼 누르기
         confirm_button.click()
 
-        if page.url == "https://service.epost.go.kr/front.commonpostplus.RetrieveRegiPostPlus.postal":
+        # 페이지가 넘어가면 접수 신청 완료
+        if (
+            page.url
+            == "https://service.epost.go.kr/front.commonpostplus.RetrieveRegiPostPlus.postal"
+        ):
             print("간편사전접수 신청이 완료되었습니다.")
 
-        time.sleep(50000)
+        # 대기
+        time.sleep(1000000)
 
         # page.close()
         # browser.close()
--- utils.py
+++ utils.py
@@ -3,14 +3,20 @@ import glob
 import os
 import json
 
+
 # 주소 파일 오류 확인
 class NoAddressFileError(Exception):
     pass
 
+
 class TooManyAddressFilesError(Exception):
     pass
 
+
 def handle_dialog(dialog):
+    """
+    :param dialog: 브라우저의 알림창  (confirm / alter 두 종류)
+    """
     dt = dialog.type
     alert_message = dialog.message
     if alert_message == "결제카드를 재검증 해주시기 바랍니다.":
@@ -23,7 +29,7 @@ def handle_dialog(dialog):
 
 def login_and_cache(context, page, file):
     """
-    Logs in manually and saves the authenticated session state to a file.
+    수동으로 로그인하고 cache 파일 저장
     """
     page.click("#loginSURL")
     # 수동 로그인 20초
@@ -34,6 +40,10 @@ def login_and_cache(context, page, file):
 
 
 def find_file():
+    """
+    주소 파일 찾는 함수
+    :return: .xls or .xlsx 엑셀 파일 하나
+    """
     xls_files = glob.glob("*.xls") + glob.glob("*.xlsx")
     if not xls_files:
         print(".xls 또는 .xlsx 형식의 파일을 찾지 못했습니다.")
@@ -45,6 +55,7 @@ def find_file():
 
     file_to_upload = xls_files[0]
     return file_to_upload
+
 
 def card_isvalid():
     card_number = input("카드 번호를 입력하세요: ").strip()
@@ -64,6 +75,7 @@ def card_month_year():
         month, year = card_month.split("/")
         return month, year
 
+
 def card_password():
     pwd = input("카드 비밀번호 앞 두 자리를 입력하세요: ").strip()
     if len(pwd) != 2:
@@ -71,6 +83,7 @@ def card_password():
         return card_password()
     else:
         return pwd[0], pwd[1]
+
 
 def birthdate():
     bd = input("카드 소유자 생년월일을 입력하세요 ex) 890403: ")
@@ -82,12 +95,12 @@ def birthdate():
 
 
 def load_card_info(card_info_file):
-    """Load card information from a JSON file."""
+    """json 형식의 카드 정보 출력"""
     if os.path.exists(card_info_file):
-        with open(card_info_file, 'r') as file:
+        with open(card_info_file, "r") as file:
             saved_card = json.load(file)
         print("현재 사용 중인 카드 정보: ")
-        keys = ['card1', 'card2', 'card3', 'card4']
+        keys = ["card1", "card2", "card3", "card4"]
         card_number, expire_date = "", ""
         for key in keys:
             card_number += saved_card[key]
@@ -100,6 +113,6 @@ def load_card_info(card_info_file):
 
 
 def save_card_info(card_info_file, card_info):
-    """Save card information to a JSON file."""
-    with open(card_info_file, 'w') as file:
+    """json 형식으로 카드 정보 저장"""
+    with open(card_info_file, "w") as file:
         json.dump(card_info, file)
